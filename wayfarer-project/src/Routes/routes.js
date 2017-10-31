9b118f356cbaa58820e9d5a39d3dd9a4fd81b270
pengfei <pengfeiye1994@gmail.com>
2017-10-31 10:58:19 -0700

    sign up page part 1

--- wayfarer-project/src/Routes/routes.js
+++ wayfarer-project/src/Routes/routes.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Route } from 'react-router'
 import Home from '../Home'
+import SignUp from '../Components/Signup'
 
 var routes = (
 	<div>
 		<Route path ='/' component={Home} />
+    <Route path='/signup' component={SignUp} />
 	</div>
 )
 
