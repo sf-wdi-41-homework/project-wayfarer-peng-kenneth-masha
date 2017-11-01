037ef7e8eceaeee91d8b7b1ead40c71f0ab4f82e
pengfei <pengfeiye1994@gmail.com>
2017-11-01 10:22:18 -0700

    backend and react linked, login modal backdrop need to be fix

--- wayfarer-project/server.js
+++ wayfarer-project/server.js
@@ -46,6 +46,18 @@ app.use(function(req, res, next) {
   global.currentUser = req.user;
   next();
   });
+//Prevent CORS errors
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+
+  //Remove caching
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+  });
+
 
 
 //User auth
