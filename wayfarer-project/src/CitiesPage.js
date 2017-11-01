ab9c9cf6f5b4b6275eff6498bb2979a3c7c8409e
marbisman <maria.arbisman@gmail.com>
2017-11-01 10:53:33 -0700

    added profileIcon as its own component and rendered it on cities and profile pages

--- wayfarer-project/src/CitiesPage.js
+++ wayfarer-project/src/CitiesPage.js
@@ -3,10 +3,24 @@ import axios from 'axios';
 import {browserHistory} from 'react-router';
 import {Link} from 'react-router';
 import './CitiesPage.css';
+import ProfileIcon from './ProfileIcon.js';
 
 class CitiesPage extends Component {
   render(){
     return(
+      <div>
+      <div className="navigationWithPic">
+      <nav className="navbar navbar-default" role="navigation">
+        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a href="#home"><ProfileIcon /></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+
       <div className="citiesPage container">
         <div className="row">
             <div className="col-md-6">
@@ -17,6 +31,7 @@ class CitiesPage extends Component {
             </div>
           </div>
         </div>
+      </div>
     )
   }
 };
--- wayfarer-project/src/ShowPage.js
+++ wayfarer-project/src/ShowPage.js
@@ -3,10 +3,24 @@ import axios from 'axios';
 import {browserHistory} from 'react-router';
 import {Link} from 'react-router';
 import './ShowPage.css';
+import ProfileIcon from './ProfileIcon';
 
 class ShowPage extends Component {
   render(){
     return(
+      <div>
+      <div className="navigationWithPic">
+      <nav className="navbar navbar-default" role="navigation">
+        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a href="#home"><ProfileIcon /></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+
     <div className="showPage container">
       <div className="row">
           <div className="col-md-6">
@@ -26,6 +40,7 @@ class ShowPage extends Component {
               <div className="col-md-6">Content</div>
           </div>
   </div>
+</div>
   )
   }
 }
